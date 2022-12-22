843a3a474c8408cbb32ca35299be2c2cf674465e
Wesley6801 <wesleywhite424@hotmail.com>
2022-12-21 19:16:43 -0500

    Continuation of adding more important files

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,9 @@
+require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
-
-
+const connectDB = require('./DB/connect')
+require('dotenv').config()
 // middleware
 
 app.use(express.json())
@@ -14,9 +15,16 @@ app.get('/hello', (req, res) =>{
 
 app.use('/api/v1/tasks', tasks)
 
-
-
-
 const port = 3000
 
-app.listen(port, console.log(`server is listening on port ${port}...`))
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
